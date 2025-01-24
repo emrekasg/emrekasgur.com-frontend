c416257ea4c7c456940662d6698a1d7fac296195
emrekasg <emrejx.abi@gmail.com>
2025-01-24 04:24:51 +0300

    export static

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next"
 
+// Add this line to enable static generation
+export const dynamic = 'force-static'
+
 async function getPosts() {
   const res = await fetch("https://backend.emrekasgur.com/posts?language=en", { next: { revalidate: 3600 } })
   if (!res.ok) {
@@ -11,7 +14,7 @@ async function getPosts() {
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const { data: posts } = await getPosts()
 
-  const postEntries = posts.map((post) => ({
+  const postEntries = posts.map((post: any) => ({
     url: `https://emrekasgur.com/posts/${post.post_link}`,
     lastModified: post.updated_at,
   }))
